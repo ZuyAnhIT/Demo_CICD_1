08af7cc41e15b119b7c40d3676eb2c3be97dffa0
ZiyAnkIT <anhtrauluoi@gmail.com>
2025-06-18 08:35:41 +0700

    The Sixth

--- obj/Debug/net9.0/HelloJenkins_1.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloJenkins_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloJenkins_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8645195e905a723b1e086c513623f2aed0a3b9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloJenkins_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloJenkins_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
